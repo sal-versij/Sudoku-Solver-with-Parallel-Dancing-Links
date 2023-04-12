6e02a908761d556e0a66e874f81cc9c2d3a16f5f
sal-versij <salvoluca1212000@gmail.com>
2023-04-12 17:50:40 +0200

    Update bulk_execution.ps1

--- bulk_execution.ps1
+++ bulk_execution.ps1
@@ -1,12 +1,17 @@
-$env:OCL_PLATFORM="0"
+$env:OCL_PLATFORM = "0"
+
+$lwss = @(1, 2, 4, 8)
 
 foreach ($i in 1..8)
 {
     $input = "./inputs/$i.txt"
-    Write-Host "Running $i"
-    foreach ($j in 0..9)
+    foreach ($lws in $lwss)
     {
-        Write-Host "Run $j"
-        .\cmake-build-debug\dlx_parallel.exe $input 1 out.csv
+        Write-Host "Running $i with $lws"
+        foreach ($j in 0..9)
+        {
+            Write-Host "."
+            $null = .\cmake-build-debug\dlx_parallel.exe $input 1 GPU2.csv
+        }
     }
 }
